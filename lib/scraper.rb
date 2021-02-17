31132a7dfc93a75fb260c68d58373a692bf880f2
Tinkergit <nicholas.stagno@gmail.com>
2021-02-17 16:37:24 -0500

    commit tree 3f9dfcf5fa93210ce30f956e7b4259189e817de6

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,11 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+#doc.css(".headline-26OIBN")
+courses = doc.css(".tout_label.heading.heading--level-4")
+
+course.each do |course|
+    puts course.text.strip
+end
